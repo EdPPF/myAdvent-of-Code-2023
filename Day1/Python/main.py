bd12003e5d2ad8069d214a4b7e11faf825759cae
EduardoPPF <ppfeduardo@gmail.com>
2023-12-11 18:21:26 -0300

    Adding documentation and comments to main.py

--- Day1/Python/main.py
+++ Day1/Python/main.py
@@ -2,10 +2,27 @@ def main():
     print(check_sum(file="values.txt"))
     # print(check_sum(file="test.txt"))
 
-# First attempt:
-# Surely, there are better ways to do this (or, at least, ways to do it eith less code).
+# First attempt: Valid
+# Surely, there are better ways to do this (or, at least, ways to do it with less code).
 # Next i might try using more built-in functions to check the numbers on each line of the input.
 def check_sum(file=""):
+    '''
+    First approach to the solution:
+        After opening the file (given the input is in a .txt), this function goes through each
+        line of it, uses two variables, `begin` and `end`, to parse the string from it's beginning
+        and ending until a number is found. These two values are used to compose a two digit number.
+        After this, these values are stored in the `lines_sum` list.
+        After all the input is used, the return is the sum of all numbers.
+
+    ### Parameters
+    `file`: `str`
+        The file name or location of the input of the problem.
+
+    ### Returns
+    `int`
+        An integer representing the sum of all the desired values from the input.
+    '''
+
     if (file == ""):
         return "File unreachable, please check if the file exists or is in the correct location."
 
