a3cab6b9f1f05a1c9a54d9be979bca036b6bdc85
EduardoPPF <ppfeduardo@gmail.com>
2023-12-16 16:08:12 -0300

    Rust Solution: First attempt - Correct

---
+++ Day1/Rust/src/main.rs
@@ -0,0 +1,46 @@
+fn main() {
+    let solution: u32 = get_sum("src/values.txt");
+    println!("Solution for 'src/values.txt': {}", solution);
+
+    let solution: u32 = get_sum("src/test.txt");
+    println!("Solution for 'src/test.txt': {}", solution);
+}
+
+use std::fs;
+
+/// First attempt: Valid
+/// This feels convoluted and i'm not sure if it's following good practices, but it works.
+fn get_sum(file: &str) -> u32 {
+    let data: String = fs::read_to_string(file).expect("Unable to read file");
+
+    let mut result: u32 = 0;
+    for line in data.lines() {
+        // This returns the index of the first numeric char
+        let first_num_index: usize = line.find(char::is_numeric).unwrap();
+        // This gets the char at the index, already as integer
+        let first_num: u32= line.chars().nth(first_num_index).unwrap().to_digit(10).unwrap();
+        // println!("[{}] = {}", first_num_index, first_num);
+
+        // This, alongside the unwrap() method, will return the index of the last numeric char
+        let last_num_index: usize = line.rfind(char::is_numeric).unwrap();
+        // This gets the char at the index, already as integer
+        let last_num: u32 = line.chars().nth(last_num_index).unwrap().to_digit(10).unwrap();
+        // println!("[{}] = {}", last_num_index, last_num);
+
+        // println!("----{}----", first_num * 10 + last_num); // Matematica
+
+        result += first_num * 10 + last_num;
+    }
+    result
+}
+
+
+// Solution Idea:
+// While parsing through the file, on each line, start from the beginning
+// and the end of the string until finding a number. Take those numbers
+// to form the two digit value.
+    // What can be used:
+        // The retain method: https://doc.rust-lang.org/std/string/struct.String.html#method.retain
+        // It takes a String and MODIFIES IT such that it retains only the specified chars. As in the
+        // example, we can use it to delete the occurences of the specified character, which could be
+        // used to retain only numeric chars.
