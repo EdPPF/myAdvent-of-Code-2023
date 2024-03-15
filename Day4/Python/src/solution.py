dfa23f5767c992bdacb172a980e16d678152d822
EdPPF <ppfeduardo@gmail.com>
2024-03-15 18:06:58 -0300

    Massive refactoring of functions

--- Day4/Python/src/solution.py
+++ Day4/Python/src/solution.py
@@ -1,39 +1,10 @@
 def main():
-    # print(get_points("test.txt", False))
-    # print(get_points("input.txt", False))
-    print(second_impact("../input.txt"))
-    # print(second_impact("Python/input.txt"))
+    print(get_points("test.txt"))
+    print(get_points("input.txt"))
+    # print(second_impact("../input.txt"))
+    # print(second_impact("../input.txt"))
 
 
-## I'm resorting to regex... For no particular reason though.
-import re
-def get_points(path:str, part_two: bool) -> int | dict[int, list[int]]:
-    with open(file=path) as arc:
-        archive = arc.read().split('\n')
-
-    total         : int                  = 0
-    returning_dict: dict[int, list[int]] = {}
-    for line in archive[:-1]:
-        points           : int       = 0
-        separate         : list[str] = line.split('|')
-        returning_numbers: list[int] = []
-
-        winning_numbers = list(map(int, re.findall(r'\d+', separate[0])))
-        game_id         : int       = winning_numbers[0]
-        winning_numbers : list[int] = winning_numbers[1:]
-        obtained_numbers: list[int] = list(map(int, re.findall(r'\d+', separate[1])))
-
-        for number in obtained_numbers:
-            if number in winning_numbers:
-                returning_numbers.append(number)
-                points = points+1 if points == 0 else points*2
-        returning_dict[game_id] = returning_numbers
-        total += points
-
-    return returning_dict if part_two else total
-
-
-#############################Part 2##############################
 class ScratchCard:
     def __init__(self, winning_nums: list[str], holding_nums: list[str], card_id: int) -> None:
         self.winning_numbers: list[str] = winning_nums
@@ -55,30 +26,83 @@ class ScratchCard:
                 counter += 1
         return counter
 
+    def points(self) -> int:
+        """
+        FOR PART 1 ONLY\n
+        Returns the number of points this card is worth.
+        """
+        points = 0
+        for num in self.holding_numbers:
+            if num in self.winning_numbers:
+                points = points+1 if points == 0 else points*2
+        return points
 
-def second_impact(path: str) -> int:
+
+def open_file(path: str) -> list[str]:
+    """
+    Auxiliar function for opening input files.\n
+    Returns:
+        `list[str]`: A list of strings, each string being a line from the file.
+    """
     with open(file=path) as arc:
         archive = arc.read().split('\n')
-    archive = archive[:-1]
+    return archive[:-1]
 
-    adict = {}
+
+def get_card_components(line: str) -> tuple[list[int], list[int], int]:
+    """
+    Auxiliar function\n
+    Parses a line of input and extracts the card components.
+
+    Args:
+        `line (str)`: The input line containing the card components.
+
+    Returns:
+        `tuple[list[int], list[int], int]`: A tuple containing the win numbers, hold numbers, and card ID.
+    """
+    separated_nums: list[str] = line.split('|')
+
+    win_nums = list(map(int, re.findall(r'\d+', separated_nums[0])))
+    card_id: int = win_nums[0]
+    win_nums: list[int] = win_nums[1:]
+    hold_nums: list[int] = list(map(int, re.findall(r'\d+', separated_nums[1])))
+
+    return win_nums, hold_nums, card_id
+
+import re
+#############################Part 1##############################
+def get_points(path:str) -> int:
+    archive = open_file(path)
+
+    total: int = 0
+    for line in archive:
+        points: int = 0
+        winning_numbers, obtained_numbers, card_id = get_card_components(line)
+
+        card = ScratchCard(winning_numbers, obtained_numbers, card_id)
+
+        points = card.points()
+        total += points
+
+    return total
+
+#############################Part 2##############################
+def second_impact(path: str) -> int:
+    archive = open_file(path)
+
+    cards_deck = {}
     scratches = []
-    for card in archive:
-        separated_nums = card.split('|')
-
-        win_nums = re.findall(r'\d+', separated_nums[0])
-        card_id = int(win_nums[0])
-        win_nums = win_nums[1:]
-        hold_nums = re.findall(r'\d+', separated_nums[1])
+    for line in archive:
+        win_nums, hold_nums, card_id = get_card_components(line)
 
         card = ScratchCard(win_nums, hold_nums, card_id)
-        adict[card] = card.get_matches()
+        cards_deck[card] = card.get_matches()
         scratches.append(card)
 
-    keys = list(adict.keys())
+    keys = list(cards_deck.keys())
     for i in range(len(keys)):
         current_card = keys[i]
-        current_times = adict[current_card]
+        current_times = cards_deck[current_card]
         if current_times == 0:
             continue
 
@@ -87,7 +111,7 @@ def second_impact(path: str) -> int:
             next_card.add_occurrence(current_card.occurrences)
 
     total = 0
-    for card in adict:
+    for card in cards_deck:
         total += card.occurrences
 
     return total
