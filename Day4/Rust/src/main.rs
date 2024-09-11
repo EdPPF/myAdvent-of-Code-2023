105ddecdc164828eddb1825fe6caeac8b4a5aef1
EdPPF <ppfeduardo@gmail.com>
2024-09-11 08:55:39 -0300

    Part 2: Rust solution working (forgot to push :/)

--- Day4/Rust/src/main.rs
+++ Day4/Rust/src/main.rs
@@ -1,10 +1,11 @@
 fn main() {
-    println!("{}", get_points("src/test.txt"));
-    println!("{}", get_points("src/input.txt"));
+    // println!("{}", get_points("src/test.txt"));
+    // println!("{}", get_points("src/input.txt"));
+    second_impact("src/test.txt");
 }
 
 
-use std::fs;
+use std::{collections::HashMap, fs};
 use regex::Regex as re;
 fn file_parser(file: &str) -> String {
     let data: String = fs::read_to_string(file).expect("Unable to read file.");
@@ -41,6 +42,91 @@ fn get_points(path: &str) -> i32 {
         total_points += game_points;
     }
 
+    total_points
+}
+
+
+//###########################Part 2##############################
+#[derive(Eq, Hash, PartialEq, Clone)]
+struct ScratchCard {
+    winning_numbers: Vec<i32>,
+    holding_numbers: Vec<i32>,
+    occurrences    : i32,
+    id             : i32,
+}
+
+impl ScratchCard {
+    fn new(id: i32, winning_numbers: Vec<i32>, holding_numbers: Vec<i32>) -> ScratchCard {
+        ScratchCard {
+            id,
+            winning_numbers,
+            holding_numbers,
+            occurrences : 0,
+        }
+    }
+
+    fn add_occurrence(&mut self, times: &i32) {
+        self.occurrences += times;
+    }
+
+    fn get_matches(&self) -> i32{
+        let mut counter: i32 = 0;
+        for num in self.holding_numbers.iter() {
+            if self.holding_numbers.contains(num) {
+                counter += 1;
+            }
+        }
+        counter
+    }
+}
+
+
+fn second_impact(path: &str) -> i32 {
+    let data: String = file_parser(path);
+    let cards: Vec<_> = data.lines().collect();
+
+    let mut card_set: HashMap<ScratchCard, i32> = HashMap::new();
+    for card in cards {
+        let separated_nums: Vec<&str> = card.split('|').collect();
+
+        let match_expression: re = re::new(r"\d+").unwrap();
+        let mut winning_numbers: Vec<&str> = match_expression
+                            .find_iter(separated_nums[0])
+                            .map(|m| m.as_str())
+                            .collect();
+
+        let card_id: i32 = winning_numbers[0].parse().unwrap();
+        winning_numbers = winning_numbers[1..].to_vec();
+        let holding_numbers: Vec<&str> = match_expression
+                        .find_iter(separated_nums[1])
+                        .map(|m| m.as_str())
+                        .collect();
+
+        let winning_numbers: Vec<i32> = winning_numbers.iter().map(|&x| x.parse().unwrap()).collect();
+        let holding_numbers: Vec<i32> = holding_numbers.iter().map(|&x| x.parse().unwrap()).collect();
+
+        let card: ScratchCard = ScratchCard::new(card_id, winning_numbers, holding_numbers);
+        card_set.insert(card, card_id);
+
+        let keys: Vec<ScratchCard> = card_set.keys().cloned().collect();
+        for i in 0..keys.len() {
+            let current_card: &ScratchCard = &keys[i];
+            let current_times: &i32 = card_set.get(current_card).unwrap();
+            if current_times == &0i32 {
+                continue;
+            }
+            for _ in 0..current_card.occurrences {
+                for j in current_card.id..(current_card.id + current_times) {
+                    let mut next_card: &ScratchCard = &keys[j as usize];
+                    next_card.add_occurrence();
+                }
+            }
+        }
+    }
+    let mut total_points: i32 = 0;
+    for card in card_set.keys() {
+        total_points += card.occurrences;
+    }
     total_points
 }
 
