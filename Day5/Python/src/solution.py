0623dfe81fe1ec858f064d63862d60a0ec738ef0
EdPPF <ppfeduardo@gmail.com>
2024-09-12 12:12:54 -0300

    Part 1: Python - Formatting code

--- Day5/Python/src/solution.py
+++ Day5/Python/src/solution.py
@@ -1,77 +1,64 @@
-def _open_file(path: str) -> list[str]:
-    with open(file=path) as arc:
-        lines = arc.read().split('\n')
-    return lines[:-1]
+def _read_file(path: str) -> list[str]:
+    file = open(path, 'r')
+    return list(
+            filter(
+                lambda x: x != '\n',
+                file.readlines())
+        )
 
 
-def _compose_maps(lines: list[str]) -> list[list[str | list[int]]]:
-    maps = []
-    current_map = []
-    lines_len = len(lines)
-    counter = 0
+def _remove_newline(line: str):
+    line = line.translate(str.maketrans('', '', '\n'))
+    return line
+
+def _remove_newline_from_list(lines: list[str]):
+    for line_idx in range(len(lines)):
+        lines[line_idx] = _remove_newline(lines[line_idx])
+
+
+def _create_range(line: str):
+    chars = line.split(' ')
+    numbers = list(map(int, chars))
+    dst_start = numbers[0]
+    src_start = numbers[1]
+    length    = numbers[2]
+    dst_range = list(range(dst_start, dst_start+length, 1))
+    src_range = list(range(src_start, src_start+length, 1))
+    return dict(zip(src_range, dst_range))
+
+def _compose_maps(lines: list[str]):
+    maps = {}
+    current = ''
     for line in lines:
-        counter += 1
-        if line == '':
-            maps.append(current_map)
-            current_map = []
-            continue
         if 'map' in line:
-            current_map.append(line)
+            current = line
+            maps[current] = {}
             continue
-        numbers = line.split(' ')
-        current_map.append(list(map(int, numbers)))
-        if counter >= lines_len:
-            maps.append(current_map)
+        maps[current] |= _create_range(line) # nice
     return maps
 
 
 #############################Part 1##############################
 def first_impact(path:str):
-    lines = _open_file(path)
+    lines = _read_file(path)
+    _remove_newline_from_list(lines)
 
-    seeds = [line for line in lines[0].split(' ') if line.isdecimal()]
+    seeds = lines[0].split(' ')[1:]
+    lines = lines[1:]
+    all_maps = _compose_maps(lines)
 
-    lines = lines[2:]
-    # print(lines)
-    maps = _compose_maps(lines)
-    # print(maps)
     corresps = []
     for seed in seeds:
+        current_src = int(seed)
         current_corresp = []
-        src = int(seed)
-        current_corresp.append(src)
-        # print(f"On seed {seed}:")
-        for map in maps:
-            # print(f"{' '*4}Current src: {src}")
-            map_len = len(map)
-            # print(f"{' '*4}Current map (len={map_len}): {map}")
-            counter = 0
-            for rang in map[1:]:
-                counter += 1
-                src_start = rang[0]
-                dst_start = rang[1]
-                length    = rang[2]
-                # print(f"{' '*4}src_start: {src_start}\n{' '*4}dst_start: {dst_start}\n{' '*4}length: {length}")
-
-                src_range = list(range(src_start, src_start+length, 1))
-                dst_range = list(range(dst_start, dst_start+length, 1))
-                # print(f"{' '*4}SRC_RANGE: {src_range}")
-                # print(f"{' '*4}DST_RANGE: {dst_range}")
-                if src in dst_range:
-                    corresp_idx = dst_range.index(src)
-                    src = src_range[corresp_idx]
-                    # print(f"{' '*8}src NOW: {src}")
-                    current_corresp.append(src)
-                    # Break on first match:
-                    break
-                else:
-                    # print(f"{' '*8} Counter: {counter}")
-                    if counter >= map_len-1:
-                        # print(f"{' '*8}src NOW: {src}")
-                        current_corresp.append(src)
-            # print(f"->> List Now: {current_corresp}")
+        current_corresp.append(current_src)
+        for maps in all_maps.values():
+            if current_src in maps.keys():
+                current_src = maps[current_src]
+                current_corresp.append(current_src)
+            else:
+                current_corresp.append(current_src)
         corresps.append(current_corresp)
-        # print(f"-> {current_corresp}")
 
     return min([corr[-1] for corr in corresps])
 
